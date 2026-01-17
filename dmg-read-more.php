20400dcb379c57660652329503d541453010020e
Dylan de Bruijn <dylandebruyn1993@gmail.com>
2026-01-17 13:25:19 +0000

    Scaffold plugin

---
+++ dmg-read-more.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name:     DMG Read More
+ * Plugin URI:      https://github.com/DylandeBruijn/dmg-read-more
+ * Description:     Gutenberg block for inserting stylized post links with WP-CLI search command
+ * Author:          Dylan de Bruijn
+ * Author URI:      https://github.com/DylandeBruijn
+ * Text Domain:     dmg-read-more
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Dmg_Read_More
+ */
