f641850472029b47331bdb0cf36eeffe7439d74d
Dylan de Bruijn <dylandebruyn1993@gmail.com>
2026-01-17 21:55:33 +0000

    Add search field with search, link and unlink functionalities. Get posts based on the search query.

--- src/blocks/post-link/edit.js
+++ src/blocks/post-link/edit.js
@@ -1,40 +1,98 @@
-/**
- * Retrieves the translation of text.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
- */
 import { __ } from '@wordpress/i18n';
-
-/**
- * React hook that is used to mark the block wrapper element.
- * It provides all the necessary props like the class name.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
- */
-import { useBlockProps } from '@wordpress/block-editor';
-
-/**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * Those files can contain any CSS code that gets applied to the editor.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
- */
+import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
+import { PanelBody, SearchControl, Spinner, Button } from '@wordpress/components';
+import { useSelect } from '@wordpress/data';
+import { useState } from '@wordpress/element';
 import './editor.scss';
 
-/**
- * The edit function describes the structure of your block in the context of the
- * editor. This represents what the editor will render when the block is used.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
- *
- * @return {Element} Element to render.
- */
-export default function Edit() {
+export default function Edit({ attributes, setAttributes }) {
+	const { postTitle, postUrl } = attributes;
+	const [searchTerm, setSearchTerm] = useState("");
+
+	const { posts, isResolving } = useSelect(
+		(select) => {
+			if (!searchTerm) {
+				return { posts: [], isResolving: false };
+			}
+
+			const { getEntityRecords, isResolving: checkResolving } = select('core');
+
+			const query = {
+				status: 'publish',
+				per_page: 5,
+				_fields: ['id', 'title', 'link'],
+				search: searchTerm,
+			};
+
+			return {
+				posts: getEntityRecords('postType', 'post', query) || [],
+				isResolving: checkResolving('getEntityRecords', ['postType', 'post', query]),
+			};
+		},
+		[searchTerm]
+	);
+
+	const handlePostUnlink = () => {
+		setAttributes({ postTitle: "", postUrl: "#" });
+		setSearchTerm("");
+	};
+
+	const handlePostSelect = (selectedPost) => {
+		setSearchTerm('');
+		setAttributes({
+			postId: selectedPost.id,
+			postTitle: selectedPost.title.rendered,
+			postUrl: selectedPost.link,
+		});
+	};
+
+	const linkText = postTitle ? `Read More: ${postTitle}` : 'Read More';
+
 	return (
-		<p {...useBlockProps({ className: 'dmg-read-more' })}>
-			<a href="#">
-				Read More
-			</a>
-		</p>
+		<>
+			<InspectorControls>
+				<PanelBody title="Settings" initialOpen={true}>
+					{postTitle && (
+						<div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginBottom: '8px' }}>
+							<span>
+								<span style={{ fontWeight: 500 }}>{__('Selected Post:', 'dmg-read-more')}</span> {postTitle}
+							</span>
+							<Button variant="link" onClick={handlePostUnlink}>Unlink</Button>
+						</div>
+					)}
+					<SearchControl
+						label={__('Search Posts', 'dmg-read-more')}
+						help={__('Search for a post to link to.', 'dmg-read-more')}
+						value={searchTerm}
+						onChange={setSearchTerm}
+						placeholder={__('Search post...', 'dmg-read-more')}
+
+					/>
+					{isResolving && <Spinner />}
+					{!isResolving && posts.length > 0 && (
+						<>
+							<span style={{ display: 'block', marginBottom: '8px', fontWeight: 500 }}>
+								Search Results:
+							</span>
+							<ul style={{ margin: 0 }}>
+								{posts.map((post) => (
+									<li
+										key={post.id}
+										style={{ margin: 0 }}
+									>
+										<Button variant="link" onClick={() => handlePostSelect(post)}>
+											{post.title.rendered || __('(No title)', 'dmg-read-more')}
+										</Button>
+									</li>
+								))}
+							</ul>
+						</>
+					)}
+				</PanelBody>
+			</InspectorControls>
+			<p {...useBlockProps({ className: 'dmg-read-more' })}>
+				<a href={postUrl}>{linkText}</a>
+			</p>
+		</>
 	);
 }
--- src/blocks/post-link/save.js
+++ src/blocks/post-link/save.js
@@ -1,24 +1,13 @@
-/**
- * React hook that is used to mark the block wrapper element.
- * It provides all the necessary props like the class name.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
- */
 import { useBlockProps } from '@wordpress/block-editor';
 
-/**
- * The save function defines the way in which the different attributes should
- * be combined into the final markup, which is then serialized by the block
- * editor into `post_content`.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#save
- *
- * @return {Element} Element to render.
- */
-export default function save() {
+export default function save({ attributes }) {
+	const { postTitle, postUrl } = attributes;
+
+	const anchorText = postTitle ? `Read More: ${postTitle}` : 'Read More';
+
 	return (
 		<p {...useBlockProps.save({ className: 'dmg-read-more' })}>
-			<a href="#">Read More</a>
+			<a href={postUrl}>{anchorText}</a>
 		</p>
 	);
 }
