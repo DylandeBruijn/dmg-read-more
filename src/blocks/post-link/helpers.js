b77657802229e93437c9be8fc813e321d7fea08c
Dylan de Bruijn <dylandebruyn1993@gmail.com>
2026-01-18 17:23:10 +0000

    Add truncate helper in case post title length will be problematic

--- src/blocks/post-link/helpers.js
+++ src/blocks/post-link/helpers.js
@@ -4,3 +4,17 @@ export function stripHtmlTags( string ) {
 	const htmlTags = /<[^>]*>?/g;
 	return decodeEntities( string ).replace( htmlTags, '' );
 }
+
+export function truncate( string, maxWords ) {
+	if ( typeof string !== 'string' ) {
+		return '';
+	}
+
+	const words = string.trim().split( /\s+/ );
+
+	if ( words.length <= maxWords ) {
+		return string;
+	}
+
+	return words.slice( 0, maxWords ).join( ' ' ) + '…';
+}
